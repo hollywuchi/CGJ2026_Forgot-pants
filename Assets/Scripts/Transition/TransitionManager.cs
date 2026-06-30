78693e6ca38b23f7a06ab307b34187295e6077a9
hollywuchi <154769557+hollywuchi@users.noreply.github.com>
2026-06-30 11:31:23 +0800

    修改冲突提交，并尝试合并

---
+++ Assets/Scripts/Transition/TransitionManager.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using Unity.Loading;
+using CGJ.Save;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CGJ.Transition
+{
+    public class TransitionManager : Singleton<TransitionManager>
+    {
+        public string startSceneName;
+
+        private CanvasGroup canvasGroup;
+        private bool isFade;
+        private AsyncOperation operation;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            // 在开始游戏之前添加UI场景
+            SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+        }
+        void OnEnable()
+        {
+            // EventHandler.TransitionEvent += OnTransitionEvent;
+            // EventHandler.StartNewGameEvent += OnStartNewGameEvent;
+            // EventHandler.EndGameEvent += OnEndGameEvent;
+        }
+        void OnDisable()
+        {
+            // EventHandler.TransitionEvent -= OnTransitionEvent;
+            // EventHandler.StartNewGameEvent -= OnStartNewGameEvent;
+            // EventHandler.EndGameEvent += OnEndGameEvent;
+        }
+
+
+
+        private void OnStartNewGameEvent(int obj)
+        {
+            StartCoroutine(LoadSaveDataScene(startSceneName));
+        }
+
+        private void OnEndGameEvent()
+        {
+            StartCoroutine(UnloadScene());
+        }
+
+        void Start()
+        {
+            canvasGroup = FindObjectOfType<CanvasGroup>();
+        }
+
+        private void OnTransitionEvent(string sceneName, Vector3 position)
+        {
+            if (!isFade)
+                StartCoroutine(Transition(sceneName, position));
+        }
+        /// <summary>
+        /// 切换场景
+        /// </summary>
+        /// <param name="sceneName">目标场景名称</param>
+        /// <param name="position">玩家落地位置</param>
+        /// <returns></returns>
+        public IEnumerator Transition(string sceneName, Vector3 position)
+        {
+
+            // EventHandler.CallBeforeSceneUnloadEvent();
+
+            yield return Fade(1);
+
+            yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+            yield return LoadSceneSetActive(sceneName);
+
+
+            // EventHandler.CallMoveToPosition(position);
+
+            // EventHandler.CallAfterSceneLoadEvent();
+
+            // 如果没有新场景没有加载完，那么就不能淡出
+            if (operation.isDone)
+                yield return Fade(0);
+        }
+
+        /// <summary>
+        /// 加载场景并激活
+        /// </summary>
+        /// <param name="sceneName">场景名称</param>
+        /// <returns></returns>
+        private IEnumerator LoadSceneSetActive(string sceneName)
+        {
+            // 异步加载场景，加载模式为添加场景
+            yield return operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+            // 获取当前加载的场景，为场景列表的最后一个
+            Scene newScene = SceneManager.GetSceneAt(SceneManager.sceneCount - 1);
+
+            SceneManager.SetActiveScene(newScene);
+        }
+
+        /// <summary>
+        /// 切换场景时的淡入淡出
+        /// </summary>
+        /// <param name="targetAlpha">1为黑，0为透明</param>
+        /// <returns></returns>
+        public IEnumerator Fade(float targetAlpha)
+        {
+            isFade = true;
+            canvasGroup.blocksRaycasts = true;
+
+            float speed = Mathf.Abs(canvasGroup.alpha - targetAlpha) / Settings.FadeDuration;
+
+            while (!Mathf.Approximately(canvasGroup.alpha, targetAlpha))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetAlpha, speed * Time.deltaTime);
+                yield return null;
+            }
+
+            isFade = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+
+        private IEnumerator LoadSaveDataScene(string sceneName)
+        {
+            yield return Fade(1);
+
+            if (SceneManager.GetActiveScene().name != "PersistentScene")
+            {
+                // EventHandler.CallBeforeSceneUnloadEvent();
+                yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+            }
+
+            yield return LoadSceneSetActive(sceneName);
+            // EventHandler.CallAfterSceneLoadEvent();
+
+            yield return Fade(0);
+        }
+
+        public IEnumerator UnloadScene()
+        {
+            // EventHandler.CallBeforeSceneUnloadEvent();
+            yield return Fade(1f);
+            yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+            yield return Fade(0);
+        }
+
+        public GameSaveData GenerateSaveData()
+        {
+            GameSaveData saveData = new GameSaveData();
+            // saveData.dataSceneName = SceneManager.GetActiveScene().name;
+
+            return saveData;
+        }
+
+        public void RestoreData(GameSaveData saveData)
+        {
+            // StartCoroutine(LoadSaveDataScene(saveData.dataSceneName));
+        }
+    }
+}
